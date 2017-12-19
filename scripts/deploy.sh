beaf3e7cf7cc34f1720d876d5495409007f75404
Victor Feight <vrfeight@gmail.com>
2017-12-19 00:33:57 -0800

    deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -e # halt script on error
 
-bundle exec htmlproofer ./_site --only-4xx --allow-hash-href --assume-extension --check-opengraph --url-ignore "feed.xml"
 zip -r website.zip _site
 
 curl -H "Content-Type: application/zip" \
