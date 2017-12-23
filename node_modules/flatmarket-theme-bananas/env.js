1264796e986fa4022edfd861ea0bd53c938ac97f
Victor Feight <vrfeight@gmail.com>
2017-12-23 01:04:15 -0800

    added older dependencies for flatmarket compatibility

--- node_modules/flatmarket-theme-bananas/env.js
+++
@@ -1,8 +0,0 @@
-/* eslint no-process-env: 0 */
-/* eslint global-require: 0 */
-
-exports.requireBrowserStyle = requireBrowserStyle
-
-function requireBrowserStyle() {
-    if (process.env.PLATFORM === 'browser') require('./index.less')
-}
--- scripts/flatmarket.sh
+++ scripts/flatmarket.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 set -e # halt script on error
 
-nvm install 4.7
-npm install flatmarket-cli
-npm install flatmarket-theme-bananas
 ./node_modules/.bin/flatmarket -c \
 	./node_modules/flatmarket-theme-bananas/index.jsx \
 	-d pay ./src/flatmarket.json
