fcd3b9e5474ec41df51e93d9d3b29ac89aea2dd2
Victor Feight <vrfeight@gmail.com>
2018-01-13 01:23:17 -0800

    change from flatmarket to stickly integration

--- scripts/flatmarket.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e # halt script on error
-
-npm install --save flatmarket-cli
-./node_modules/.bin/flatmarket -c \
-	./node_modules/flatmarket-theme-bananas/index.jsx \
-	-d pay ./src/flatmarket.json
